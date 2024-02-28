5306a9d363885e283f8aaa2d4e5fc56739e44308
Benjamin Siler <bengsiler@gmail.com>
2024-02-27 21:45:38 -0600

    Refactored backstage_tick to use a new class, Backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -46,13 +46,8 @@ class GildedRose
   end
 
   def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+    @item = Backstage.new(quality, days_remaining)
+    @item.tick
   end
   class Normal
     attr_reader :quality, :days_remaining
@@ -94,4 +89,20 @@ class GildedRose
     end
   end
 
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
 end
