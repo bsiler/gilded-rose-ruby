861335376688cb167032814395de62717e0c423d
Benjamin Siler <bengsiler@gmail.com>
2024-02-27 21:59:29 -0600

    Changed GildedRose attributes to only have :item, added klass_for and changed tick, quality, and days remaining methods to be much simpler " ;

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,54 +1,35 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :item
 
   def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+    @item = klass_for(name).new(quality, days_remaining)
   end
 
-  def tick
-    case @name
+  def klass_for(name)
+    case name
     when "Normal Item"
-      return normal_tick
+      Normal
     when "Aged Brie"
-      return brie_tick
+      Brie
     when "Sulfuras, Hand of Ragnaros"
-      return sulfuras_tick
+      Sulfuras
     when "Backstage passes to a TAFKAL80ETC concert"
-      return backstage_tick
+      Backstage
     end
   end
 
+  def tick
+    @item.tick
+  end
+
   def quality
-    return @item.quality if @item
-    @quality
+    @item.quality
   end
 
   def days_remaining
-    return @item.days_remaining if @item
-    @days_remaining
+    @item.days_remaining
   end
 
-  def normal_tick
-    @item = Normal.new(quality, days_remaining)
-    @item.tick
-  end
-
-  def brie_tick
-    @item = Brie.new(quality, days_remaining)
-    @item.tick
-  end
-
-  def sulfuras_tick
-    @item = Sulfuras.new(quality, days_remaining)
-    @item.tick
-  end
-
-  def backstage_tick
-    @item = Backstage.new(quality, days_remaining)
-    @item.tick
-  end
   class Normal
     attr_reader :quality, :days_remaining
 
@@ -74,8 +55,7 @@ class GildedRose
       @days_remaining -= 1
       return if @quality >= 50
       @quality += 1
-      return if @quality >= 50
-      @quality += 1 if @days_remaining <= 0
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
   end
 
